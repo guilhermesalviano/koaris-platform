8144a604512f792ce8118841172ae4439c3801a1
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2023-09-03 01:36:41 -0300

    feat: creating LP

--- front-control-panel/app/landingPage/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function LandingPage() {
-    return (
-      <h1>Essa é minha landing</h1>
-    )
-  }
-  
--- front-control-panel/app/page.tsx
+++ front-control-panel/app/page.tsx
@@ -1,7 +1,26 @@
-import LandingPage from "./landingPage/page";
+import Image from "next/image";
+import { Button } from "../components/Button";
 
-export default function IndexPage() {
+export default function Landing() {
   return (
-    <LandingPage />
+    <div className="flex flex-1 p-8 ">
+      <header className="flex flex-col w-11/12">
+        <nav className="flex flex-row items-center justify-between">
+          <Image src="/logo.png" width={104} height={104} alt="logo" />
+          <ul className="flex flex-row justify-between items-center">
+            <li>Início</li>
+            <li>Sobre</li>
+            <li>Serviços</li>
+            <li>Novidades</li>
+            <li>Contatos</li>
+          </ul>
+          <Button text="Login" />
+        </nav>
+        <div className="flex py-8 items-center justify-center">
+          <h1 className="text-5xl font-bold tracking-widest leading-snug ">Transformando Ideias em Soluções Digitais de Excelência</h1>
+          <Image src="/banner.png" width={720} height={720} sizes="100%" alt="Banner" />
+        </div>
+      </header>
+    </div>
   )
 }
