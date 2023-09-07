08d276c3be0ace7c14fe10c380a451a0d6b8fe80
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2023-09-07 10:52:38 -0300

    refactor: cards width

--- front-control-panel/app/page.tsx
+++ front-control-panel/app/page.tsx
@@ -4,11 +4,11 @@ import { SecundaryButton } from "../components/SecundaryButton";
 
 export default function Landing() {
   return (
-    <div className="container flex-1 flex flex-col p-14">
-      <header className="flex flex-col w-11/12">
+    <div className="container flex flex-col">
+      <header className="flex flex-col w-11/12 p-14">
         <nav className="flex flex-row items-center justify-between px-4">
           <Image src="/logo.svg" width={104} height={104} alt="logo" />
-          <ul className="flex space-x-8 text-lg font-bold">
+          <ul className="space-x-8 text-lg font-bold md:flex hidden">
             <li>Início</li>
             <li>Sobre</li>
             <li>Serviços</li>
@@ -19,7 +19,7 @@ export default function Landing() {
         </nav>
         <div className="flex py-10 items-center flex-col-reverse lg:flex-row">
           <div className="flex flex-col">
-            <h1 className="text-5xl font-bold tracking-wider leading-tight">
+            <h1 className="md:text-5xl text-4xl font-bold tracking-wider md:leading-tight">
               Transformando ideias em soluções digitais de excelência
             </h1>
             <div className="flex py-4">
@@ -32,30 +32,35 @@ export default function Landing() {
           <Image src="/banner.svg" className="lg:w-1600 w-4/6 py-8" width={1600} height={1600} sizes="100%" alt="Banner" />
         </div>
       </header>
-      <main>
-        <div className="reasons flex items-center justify-around">
-          <div className="card rounded-lg p-8 w-4/12 flex flex-col">
-            <img src="/card1.svg" alt="Inovação" className="py-4 w-3/5 self-center" />
-            <h2 className="font-bold text-lg text-left">Inovação Constante</h2>
-            <p className="text-base">
-              Estamos na vanguarda da tecnologia, mantendo nossa equipe atualizada com as últimas tendências para oferecer soluções inovadoras e alinhadas com o futuro.
-            </p>
-          </div>
+      <main className="w-full">
+        <div className="reasons flex flex-col items-center justify-around p-4">
+          <h1 className="md:text-5xl text-4xl font-bold tracking-wider md:leading-tight">
+            Por que escolher a Koaris?
+          </h1>
+          <div className="cards flex flex-col md:flex-row items-center py-4">
+            <div className="card rounded-lg p-8 flex flex-col m-2 md:w-4/12">
+              <img src="/card1.svg" alt="Inovação" className="py-2 w-200 self-center" />
+              <h2 className="font-bold text-lg text-left">Inovação Constante</h2>
+              <p className="text-base">
+                Estamos na vanguarda da tecnologia, mantendo nossa equipe atualizada com as últimas tendências para oferecer soluções inovadoras e alinhadas com o futuro.
+              </p>
+            </div>
 
-          <div className="card rounded-lg p-8 w-4/12 flex flex-col">
-            <img src="/card2.svg" alt="Inovação" className="py-4 w-3/5 self-center" />
-            <h2 className="font-bold text-lg text-left">Soluções Sob Medida</h2>
-            <p className="text-base">
-              Cada negócio é único. Desenvolvemos soluções personalizadas que impulsionam o crescimento e proporcionam vantagem competitiva.
-            </p>
-          </div>
+            <div className="card rounded-lg p-8 flex flex-col m-2 md:w-4/12">
+              <img src="/card2.svg" alt="Inovação" className="py-10 w-200 self-center" />
+              <h2 className="font-bold text-lg text-left">Soluções Sob Medida</h2>
+              <p className="text-base">
+                Cada negócio é único. Desenvolvemos soluções personalizadas que impulsionam o crescimento e proporcionam vantagem competitiva.
+              </p>
+            </div>
 
-          <div className="card rounded-lg p-8 w-4/12 flex flex-col">
-            <img src="/card3.svg" alt="Inovação" className="py-4 w-3/5 self-center" />
-            <h2 className="font-bold text-lg text-left">Suporte Contínuo</h2>
-            <p className="text-base">
-              Nossa parceria não termina no lançamento. Oferecemos suporte técnico contínuo para garantir o funcionamento perfeito e crescimento contínuo de sua solução.
-            </p>
+            <div className="card rounded-lg p-8 flex flex-col m-2 md:w-4/12">
+              <img src="/card3.svg" alt="Inovação" className="py-4 w-200 self-center" />
+              <h2 className="font-bold text-lg text-left">Suporte Contínuo</h2>
+              <p className="text-base">
+                Nossa parceria não termina no lançamento. Oferecemos suporte técnico contínuo para garantir o funcionamento perfeito e crescimento contínuo de sua solução.
+              </p>
+            </div>
           </div>
         </div>
       </main>
