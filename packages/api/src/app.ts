7f6cef53cb06c90b2d74f8e7fda765f0f6df5111
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2023-12-24 16:59:58 -0300

    fix(api): build command

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import routes from './routes';
 import "dotenv/config";
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swaggerOptions';
 
 const app = express()
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+if (process.env.NODE_ENV === 'development') {
+    const swaggerUi = require('swagger-ui-express');
+    const swaggerDocument = require('../swaggerOptions');
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 app.use(routes);
 
 export default app;
