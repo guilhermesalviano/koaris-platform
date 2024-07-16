a03e7dc2e95a0744acf5825057608ee5725fcab9
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2024-07-16 10:09:49 -0300

    feat(web): dashboard

--- packages/web/app/dashboard/page.tsx
+++ packages/web/app/dashboard/page.tsx
@@ -1,21 +1,38 @@
 import Image from "next/image";
+import { Link, Heading, Button, Text, Card } from "@koaris/bloom-ui";
 import { FiUsers, FiMessageSquare } from "react-icons/fi";
 
 export default function Dashboard() {
   return (
-    <div className="bg-dashboard-color w-full text-white flex h-screen">
-      <aside className="p-4 w-60">
-        <Image src="/logo.svg" width={104} height={104} alt="logo" />
-        <nav className="">
-          <ul className="flex align-middle justify-center flex-col">
-            <li className="text-white">Home</li>
-            <li className="text-white">Usuários</li>
-            <li className="text-white">Produtos</li>
-            <li className="text-white">Configurações</li>
+    <div className="bg-dashboard-color w-full flex justify-center h-screen">
+      <aside className="p-4 w-60 flex justify-center flex-col">
+        <Image src="/logo-negative.svg" width={104} height={104} alt="logo" className="self-center"/>
+        <nav className="h-full">
+          <ul className="h-full flex align-middle justify-center flex-col gap-6">
+            <li className="text-white">
+              <Link url="/dashboard" newPage={false} className="text-white">
+                Home
+              </Link>
+            </li>
+            <li className="text-white">
+              <Link url="/users" newPage={false} className="text-white">
+                Usuários
+              </Link>
+            </li>
+            <li className="text-white">
+              <Link url="/products" newPage={false} className="text-white">
+                Produtos
+              </Link>
+            </li>
+            <li className="text-white">
+              <Link url="/settings" newPage={false} className="text-white">
+                Configurações
+              </Link>
+            </li>
           </ul>
         </nav>
       </aside>
-      <div className="w-full h-screen bg-background rounded-2xl">
+      <div className="w-full h-screen bg-background rounded-2+xl">
         <header className="border-b-2 p-6 flex justify-end">
           <h1 className="px-2">Conta:</h1>
           <ul>
@@ -23,7 +40,7 @@ export default function Dashboard() {
           </ul>
         </header>
         <main className="flex flex-col justify-center ">
-          <h1 className="text-2xl font-bold p-8">Seja bem vindo, Guilherme</h1>
+          <Heading className="text-3xl font-bold p-8">Seja bem vindo, Guilherme</Heading>
           <div className="grid grid-cols-2">
             <div className="flex flex-col px-8">
               <div className="info-widget flex items-center py-2">
@@ -35,13 +52,20 @@ export default function Dashboard() {
                   <p className="text-black">1</p>
                 </div>
               </div>
-              <div className="dashboard-card my-8 bg-primary-color rounded-xl p-4 max-w-xs h-52">
-                <h2 className="text-base py-2 font-bold">Confirmar meu e-mail</h2>
-                <p className="text-sm py-2 w-44">Te enviamos um e-mail de confirmação, é importante confirma-lo antes de usar a plataforma.</p>
-                <button type="button" className="secundary-button my-1 rounded-md px-5 py-1">Re-enviar</button>
-              </div>
+              <Card 
+                title="Confirmar meu e-mail" 
+                content="Te enviamos um e-mail de confirmação, é importante confirma-lo antes de usar a plataforma."
+                imageSize="100px" image="/mailbox.svg" 
+                className="my-8 p-8 h-48 flex-row-reverse gap-4"
+                >
+                <Button variant="secondary" 
+                  className="secundary-button my-1 w-30" 
+                  size="sm">
+                    Re-enviar
+                </Button>
+              </Card>
               <div className="news py-6">
-                <h2 className="text-2xl font-bold text-black">Novidades</h2>
+                <Heading className="text-3xl font-bold">Novidades</Heading>
                 <div className="news-card rounded-xl">
                   <h3 className="text-sm text-black py-4">Destaque-se na Multidão Digital: Como Ser Relevante na Internet!</h3>
                   <p className="text-sm text-gray-500">Neste mundo cada vez mais conectado, ser relevante na internet é essencial para se destacar entre a multidão digital. Com a imensa quantidade de informações disponíveis online, é fundamental saber como chamar a atenção do público...</p>
@@ -61,11 +85,19 @@ export default function Dashboard() {
                   <p className="text-black">1</p>
                 </div>
               </div>
-              <div className="dashboard-card my-8 bg-primary-color rounded-xl p-4 max-w-xs h-52">
-                <h2 className="text-base py-2 font-bold">Site Institucional</h2>
-                <p className="text-sm py-4 w-44">Destaque a sua marca com um site institucional profissional e moderno!</p>
-                <button type="button" className="secundary-button my-1 rounded-md px-5 py-1">Orçamento</button>
-              </div>
+              <Card 
+                title="Site Institucional" 
+                content="Destaque a sua marca com um site institucional profissional e moderno!"
+                imageSize="100px" 
+                image="/site_institucional.svg" 
+                className="my-8 p-8 h-48 flex-row-reverse gap-4"
+                >
+                <Button variant="secondary" 
+                  className="secundary-button my-1 w-30" 
+                  size="sm">
+                    Re-enviar
+                </Button>
+              </Card>
               <div className="news py-6">
                 <h2 className="text-2xl font-bold text-black"></h2>
                 <div className="news-card rounded-xl py-4">
