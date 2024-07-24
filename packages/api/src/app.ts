2fb62dcc42a7e5e57fe577d3487be806d30aa56d
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2024-07-24 18:24:48 -0300

    feat(web): adding login integration

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -2,9 +2,16 @@ import express from 'express';
 import routes from './routes';
 import path from 'path';
 import dotenv from 'dotenv';
+import cors from "cors";
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 const app = express()
+
+app.use(
+	cors({
+        origin: process.env.FRONTURL || "http://localhost:3000",
+	})
+);
 
 app.use(express.json());
 
@@ -13,6 +20,7 @@ if (process.env.NODE_ENV === 'development') {
     const swaggerDocument = require('../swaggerOptions');
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
+
 app.use(routes);
 
 export default app;
--- packages/web/app/login/page.tsx
+++ packages/web/app/login/page.tsx
@@ -1,24 +1,47 @@
 "use client"
-import { Button, Heading, Input, Link, Text } from "@koaris/bloom-ui";
+import { Button, Form, Heading, Input, Link, Text } from "@koaris/bloom-ui";
 import { Header } from "../../components/Header";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
 
 export default function Login() {
+  const router = useRouter()
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    try {
+      event.preventDefault()
+
+      const formData = new FormData(event.currentTarget)
+      const email = formData.get('email')
+      const password = formData.get('password')
+
+      const response = await api.post('/login',
+        { email, password },
+      )
+      const { access_token } = response.data
+      console.log(access_token)
+    } catch (error: any) {
+      console.log(error.response)
+    }
+  }
+
   return (
     <div className="w-full bg-background flex justify-center h-screen">
       <div className="w-full h-screen rounded-2xl">
         <Header />
         <main className="flex items-center justify-center pt-20 h-full pl-32 pr-32">
-          <div className="flex flex-col rounded-md border shadow p-8 gap-2 min-w-[400px] max-w-[680px] w-full">
+          <Form onSubmit={handleSubmit} className="flex flex-col bg-neutral border-neutral rounded-md border shadow p-8 gap-2 min-w-[400px] max-w-[680px] w-full">
             <Heading className="sm:text-2xl text-xl font-bold text-left">Login</Heading>
             <Text tag="label">E-mail</Text>
-            <Input error={false} type="text" placeholder="guilhermesalviano177@gmail.com" className="shadow-md"></Input>
+            <Input error={false} type="text" name="email" placeholder="guilhermesalviano177@gmail.com" className="shadow-md text-neutral-800"></Input>
             <Text tag="label">Senha</Text>
-            <Input error={false} type="text" placeholder="********" className="shadow-md"></Input>
+            <Input error={false} type="password" name="password" placeholder="********" className="shadow-md text-neutral-800"></Input>
             <Link url="/recovery" newPage={false} className="text-orange-500 font-normal">Esqueci minha senha</Link>
             <div className="w-36 flex self-end">
               <Button>Login</Button>
             </div>
-          </div>
+          </Form>
         </main>
       </div>
     </div>
